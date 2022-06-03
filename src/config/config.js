da43a59943e2fb1982ed0bae074bd26555f0d0d9
Vahe <71688163+VaheMeekz@users.noreply.github.com>
2022-06-03 18:32:46 +0400

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://newcake.herokuapp.com/api/v1";
-export const baseUrl = "http://localhost:5000/api/v1";
+ export const baseUrl = "https://newcake.herokuapp.com/api/v1";
+//export const baseUrl = "http://localhost:5000/api/v1";
 export const token = JSON.parse(localStorage.getItem("myToken"));
 export const email = JSON.parse(localStorage.getItem("email"));
 export const CLOUDINARY_PRESENT = "qtfygrwo";
